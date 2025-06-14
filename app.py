8763c92943281fdc82dfab1d1e836f11e5aaf753
Ruthishkumar G <ruthishkumar.2353045@srec.ac.in>
2025-06-14 21:32:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def get_reply():
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
